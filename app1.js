00fc62c6810d0b46431ff0b7b65c3f37a3fef14e
Nullite <expteam-cs@yandex.ru>
2021-09-04 15:46:21 +0300

    commit 1

--- app1.js
+++ app1.js
@@ -1,5 +1,5 @@
 const out1 = document.querySelector('.out1')
-for (let i = 1; i < 10; i++) {
+for (let i = 2; i < 10; i++) {
     for (let j = 2; j < 6; j++)
         out1.innerHTML += `${i}^${j} = ${(i ** j)} <br>`
     out1.innerHTML += '<hr>'
