74cf85b1c0009f0a5e8b54596930db86519ac697
Nullite <90028034+Nullite@users.noreply.github.com>
2021-11-05 14:26:56 +0300

    Delete app1.js

--- app1.js
+++
@@ -1,6 +0,0 @@
-const out1 = document.querySelector('.out1')
-for (let i = 2; i < 10; i++) {
-    for (let j = 2; j < 6; j++)
-        out1.innerHTML += `${i}^${j} = ${(i ** j)} <br>`
-    out1.innerHTML += '<hr>'
-}
