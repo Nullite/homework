49874a28277c87813a5b2c57b2e4ce2508ede9ee
Nullite <90028034+Nullite@users.noreply.github.com>
2021-12-03 07:43:50 +0300

    Add files via upload

---
+++ Every Hunter Wants to Know/Every Hunter Wants to Know/Every Hunter Wants to Know.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Каждый\n"
+		    "\tОхотник\n"
+		    "\t\tЖелает\n"
+		    "\t\t\tЗнать,\n"
+		    "\t\t\t\tГде\n"
+		    "\t\t\t\t\tСидит\n"
+		    "\t\t\t\t\t\tФазан.\n";
+	return 0;
+
+};
+
