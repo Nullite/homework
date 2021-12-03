15c570b2e2f13f354877c65ba907065d1a31a2ab
Nullite <90028034+Nullite@users.noreply.github.com>
2021-12-03 11:59:41 +0300

    Add files via upload

---
+++ Circle Area/Circle Area/Circle Area.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    const float pi = 3.14159;
+    float L = 0;
+    int i;
+    cout << "Введите длинну окружности.\n";
+    cin >> L;
+    if (L <= 0) {
+        cout << "Неверно введена длина.\n ";
+        return 0;
+    };
+
+    const float R = L / (2 * pi);
+    const float S = pi * pow(R, 2);
+
+    cout << "Площадь окружности равна " << S << "\n";
+
+    return 0;
+
+};
