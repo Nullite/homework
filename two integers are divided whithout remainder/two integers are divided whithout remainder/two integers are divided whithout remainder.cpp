fda2a7595c27df109b60887003dff866b29c9df7
Nullite <90028034+Nullite@users.noreply.github.com>
2021-12-20 13:16:18 +0300

    Add files via upload

---
+++ two integers are divided whithout remainder/two integers are divided whithout remainder/two integers are divided whithout remainder.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    
+    int firstNum = 0;
+    int secondNum = 0;
+    int checkNum = 1;
+    string collectNums;
+
+    cout << "Введите два целых числа в диапазоне от -999999999 до 999999999\n"
+        << "и программа посчитает, на какие числа они оба делятся без остатка.\n";
+    cout << "Первое число: \n";
+    cin >> firstNum;
+
+    if (firstNum < -999999999 || firstNum > 999999999)
+    {
+        cout << "Число должно находится в заданном диапазоне.\n";
+        return 0;
+    }
+
+    cout << "Второе число: \n";
+    cin >> secondNum;
+
+    if (secondNum < -999999999 || secondNum > 999999999)
+    {
+        cout << "Число должно находится в заданном диапазоне.\n";
+        return 0;
+    }
+    
+
+    if (!firstNum && !secondNum)
+    {
+        cout << "нули делятся без остатка на все числа.\n";
+        return 0;
+    }
+    if (!firstNum)
+    {
+        if (secondNum < 0)
+        {
+            secondNum = -secondNum;
+        }
+        {
+            while (checkNum <= secondNum)
+            {
+                if (!(secondNum % checkNum))
+                {
+                    collectNums += to_string (checkNum) + ", ";
+                    collectNums += to_string(-checkNum)  + ", ";
+                    
+                }
+                checkNum++;
+                
+            }
+        }
+    }
+
+    else if (!secondNum)
+    {
+        if (firstNum < 0)
+        {
+            firstNum = -firstNum;
+        }
+        {
+            while (checkNum <= firstNum)
+            {
+                if (!(firstNum % checkNum))
+                {
+                    collectNums += to_string(checkNum) + ", ";
+                    collectNums += to_string(-checkNum) + ", ";
+                    
+                }
+                checkNum++;
+                
+            }
+        }
+    }
+
+    else
+    {
+        if (firstNum < 0)
+        {
+            firstNum = -firstNum;
+        }
+        if (secondNum < 0)
+        {
+            secondNum = -secondNum;
+        }
+        while (checkNum <= firstNum && checkNum <= secondNum)
+        {
+            if (!(firstNum % checkNum) && !(secondNum % checkNum))
+            {
+                collectNums += to_string(checkNum) + ", ";
+                collectNums += to_string(-checkNum) + ", ";
+                checkNum++;
+            }
+            checkNum++;
+        }
+    }
+    cout << collectNums.substr(0, collectNums.length() - 2) << "." << endl;
+}
+
+
