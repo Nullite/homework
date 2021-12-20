9b6e40d247aa2fa23adb40c21997c291da72563a
Nullite <90028034+Nullite@users.noreply.github.com>
2021-12-20 13:14:54 +0300

    Add files via upload

---
+++ number is divided whithot remainder/number is divided whithot remainder/number is divided whithot remainder.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    setlocale(LC_ALL, "rus");
+
+    int num = 0;
+    int checkNum = 1;
+    string collectNums;
+   
+
+    cout << "Введите целое число в диапазоне от -999999999 до 999999999 и программа рассчитает\n"
+        << "все числа, на которые это число делится без остатка.\n";
+    cin >> num;
+
+    if (num < -999999999 || num > 999999999)
+    {
+        cout << "Число должно быть в заданном диапазоне.\n";
+        return 0;
+    }
+    
+    if (!num)
+    {
+        cout << "0 делится без остатка на все числа в диапазоне от -infinity до +infinity.\n";
+        return 0;
+    }
+
+    if (num < 0)
+    {
+        num = -num;
+    }
+
+        while (checkNum <= num)
+        {
+            if (!(num % checkNum))
+            {
+                collectNums += to_string(checkNum) + ", ";
+                collectNums += to_string(-checkNum) + ", ";
+            }
+            checkNum++;
+        }
+       
+    
+
+        cout << collectNums.substr(0, collectNums.length() - 2) << "." << endl;
+        return 0;
+    
+}
+
