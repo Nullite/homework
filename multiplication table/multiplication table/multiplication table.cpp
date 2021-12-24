03d5207caee4bcfabeb8c2877f9137f8d8d7caeb
Nullite <90028034+Nullite@users.noreply.github.com>
2021-12-24 16:53:51 +0300

    Add files via upload

---
+++ multiplication table/multiplication table/multiplication table.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int k = 0;
+
+    cout << "Введите число от 1 до 9 и программа напишет Вам таблицу умножения на это число.\n";
+    cin >> k;
+
+    if (k < 1 || k > 9)
+    {
+        cout << "Число должно быть в диапазоне от 1 до 9.\n";
+        return 0;
+    }
+
+    string table;
+
+    for (int i = 1; i <= 9; i++)
+    {
+        table += to_string(k) + " X " + to_string(i) + " = " + to_string (k*i) + "\n";
+    }
+
+    cout << table;
+
+}
