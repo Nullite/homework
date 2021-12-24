d1aef192f80f8a15388c04fadea4027aef7067dd
Nullite <90028034+Nullite@users.noreply.github.com>
2021-12-24 16:50:36 +0300

    Add files via upload

---
+++ arithmetic mean from 1 to 1000/arithmetic mean from 1 to 1000/arithmetic mean from 1 to 1000.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+
+	int sum = 0;
+
+	for (int i = 1; i <= 1000; i++)
+	{
+		sum += i;
+	}
+
+	sum /= 1000;
+
+	cout << sum << endl;
+
+}
+
