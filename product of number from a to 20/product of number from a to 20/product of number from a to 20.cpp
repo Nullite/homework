95c4c799f07e1b05676fceeb5a04d418c33d813b
Nullite <90028034+Nullite@users.noreply.github.com>
2021-12-24 16:55:08 +0300

    Add files via upload

---
+++ product of number from a to 20/product of number from a to 20/product of number from a to 20.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int a = 0;
+    double product = 1;
+
+    cout << "Введите число от 1 до 20 и программа вернет произведение всех чисел от введенного числа до 20.\n";
+    cin >> a;
+
+    if (a < 1 || a > 20)
+    {
+        cout << "Число должно быть в диапазоне от 1 до 20.\n";
+        return 0;
+    }
+
+    for (; a <= 20; a++)
+    {
+        product *= a;
+    }
+
+    string productStr = to_string(product);
+
+
+    cout <<productStr.substr(0, productStr.length() - 7) << endl;
+    
+}
+
