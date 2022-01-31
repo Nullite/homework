02aebf30e550b865721c402dde4578284f52091c
Nullite <90028034+Nullite@users.noreply.github.com>
2022-01-31 11:05:31 +0300

    Add files via upload

---
+++ function pow/function pow/function pow.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+
+double repeatPow(double num, double deg)
+{
+    cout <<num << " raised to degree "<< deg << " is "<< pow(num, deg);
+    return pow(num, deg);
+}
+
+int main()
+{
+    double number, degree;
+
+    cout << "Enter a Number\n";
+    cin >> number;
+
+    cout << "Enter a degree\n";
+    cin >> degree;
+
+    repeatPow(number, degree);
+   
+}
+
