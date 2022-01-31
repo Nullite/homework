6f989510e58a672baf40dacf41730ab9530ba388
Nullite <90028034+Nullite@users.noreply.github.com>
2022-01-31 11:09:31 +0300

    Add files via upload

---
+++ sum in range between integers/sum in range between integers/sum in range between integers.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using std::cin;
+using std::cout;
+
+int rangeSum(int firstNum, int secondNum)
+{
+	int result = 0;
+
+	if (firstNum > secondNum)
+	{
+		int temp = secondNum;
+		secondNum = firstNum;
+		firstNum = temp;
+	}
+
+	for (int i = firstNum + 1; i < secondNum; i++)
+	{
+		result += i;
+	}
+	return result;
+}
+
+int main()
+{
+	cout << rangeSum(4, 10);
+}
+
